336129393aab737dcb7526d9d1a362a8823b1663
Markus <marce@marce.at>
2014-06-06 11:24:41 +0200

    typo

--- ActiveStruts/Config.cs
+++ ActiveStruts/Config.cs
@@ -13,7 +13,7 @@ namespace ActiveStruts
         public const float WeakJointStrength = 50;
         public const float NormalJointStrength = 2000;
         public const float MaximalJointStrength = 40000;
-        public const string LinkHelpText = "Click left on a possible target to establish a link. Press 'x' to abort. You can alos right click -> 'Set as Target' on a valid target and right click -> 'Abort' on the targeter.";
+        public const string LinkHelpText = "Click left on a possible target to establish a link. Press 'x' to abort. You can also right click -> 'Set as Target' on a valid target and right click -> 'Abort' on the targeter.";
         public const string FreeAttachHelpText = "Click left on a valid position to establish a link. Click right to abort.";
         public const float ConnectorDimension = 0.025f;
         public const float ColorTransparency = 0.5f;
