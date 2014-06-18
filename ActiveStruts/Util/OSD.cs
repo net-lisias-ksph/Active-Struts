3727019a50813086f7430ef0b8c22ec4450dae2b
Markus <marce@marce.at>
2014-06-18 15:57:38 +0200

    updated OSD class

--- ActiveStruts/Util/OSD.cs
+++ ActiveStruts/Util/OSD.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 
 namespace ActiveStruts.Util
 {
-    /*  Copyright (C) 2013 FW Industries
+    /* Modified by marce 2014
+     *  
+     * Copyright (C) 2013 FW Industries
     Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
     The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
@@ -14,19 +16,26 @@ namespace ActiveStruts.Util
     public static class OSD
     {
         private const string Prefix = "[ActiveStruts] ";
+        private static readonly object ListLock = new object();
 
         private static readonly List<Message> Msgs = new List<Message>();
 
         public static void AddMessage(String text, Color color, float shownFor)
         {
-            var msg = new Message {Text = Prefix + text, Color = color, HideAt = Time.time + shownFor};
-            Msgs.Add(msg);
+            lock (ListLock)
+            {
+                var msg = new Message {Text = Prefix + text, Color = color, HideAt = Time.time + shownFor};
+                Msgs.Add(msg);
+            }
         }
 
         private static float CalcHeight()
         {
-            var style = CreateStyle(Color.white);
-            return Msgs.Aggregate(.0f, (a, m) => a + style.CalcSize(new GUIContent(m.Text)).y);
+            lock (ListLock)
+            {
+                var style = CreateStyle(Color.white);
+                return Msgs.Aggregate(.0f, (a, m) => a + style.CalcSize(new GUIContent(m.Text)).y);
+            }
         }
 
         private static GUIStyle CreateStyle(Color color)
@@ -52,15 +61,18 @@ namespace ActiveStruts.Util
 
         public static void Update()
         {
-            if (Msgs.Count == 0)
+            lock (ListLock)
             {
-                return;
+                if (Msgs.Count == 0)
+                {
+                    return;
+                }
+                Msgs.RemoveAll(m => Time.time >= m.HideAt);
+                var h = CalcHeight();
+                GUILayout.BeginArea(new Rect(0, Screen.height*0.1f, Screen.width, h), CreateStyle(Color.white));
+                Msgs.ForEach(m => GUILayout.Label(m.Text, CreateStyle(m.Color)));
+                GUILayout.EndArea();
             }
-            Msgs.RemoveAll(m => Time.time >= m.HideAt);
-            var h = CalcHeight();
-            GUILayout.BeginArea(new Rect(0, Screen.height*0.1f, Screen.width, h), CreateStyle(Color.white));
-            Msgs.ForEach(m => GUILayout.Label(m.Text, CreateStyle(m.Color)));
-            GUILayout.EndArea();
         }
 
         public static void Warn(String text, float showDuration = 3.7f)
@@ -68,7 +80,7 @@ namespace ActiveStruts.Util
             AddMessage(text, XKCDColors.Yellow, showDuration);
         }
 
-        private class Message
+        private struct Message
         {
             public Color Color { get; set; }
             public float HideAt { get; set; }
