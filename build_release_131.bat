a4f8642ce9be91f100bf2f463647ebfae37a7dd8
meirumeiru <33137857+meirumeiru@users.noreply.github.com>
2018-04-06 18:07:39 +0200

    update

--- build_release_131.bat
+++ build_release_131.bat
@@ -14,4 +14,5 @@ copy "Resources\GameData\MagicSmokeIndustries\IRActiveStruts\ActiveStruts_1.3.ve
 
 copy "ActiveStruts\ActiveStruts\bin\Release 1.3.1\ActiveStruts.dll" "_release\GameData131\GameData\MagicSmokeIndustries\IRActiveStruts\Plugins\ActiveStruts.dll"
 
+del "_release\IR-ActiveStruts_v3.0.0_for_1.3.1.zip"
 C:\PACL\PACOMP.EXE -a -r -p "_release\IR-ActiveStruts_v3.0.0_for_1.3.1.zip" "_release\GameData131\*"
--- build_release_142.bat
+++ build_release_142.bat
@@ -14,4 +14,5 @@ copy "Resources\GameData\MagicSmokeIndustries\IRActiveStruts\ActiveStruts_1.3.ve
 
 copy "ActiveStruts\ActiveStruts\bin\Release 1.4.2\ActiveStruts.dll" "_release\GameData142\GameData\MagicSmokeIndustries\IRActiveStruts\Plugins\ActiveStruts.dll"
 
+del "_release\IR-ActiveStruts_v3.0.0_for_1.4.2.zip"
 C:\PACL\PACOMP.EXE -a -r -p "_release\IR-ActiveStruts_v3.0.0_for_1.4.2.zip" "_release\GameData142\*"
